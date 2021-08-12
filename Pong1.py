dea6a019dd505a4879f7fbaa55bc4abae85ca3d5
Ramadhani Mdee <42729286+ramsony@users.noreply.github.com>
2021-08-11 20:03:19 -0400

    settiong up pen to display name and score board

--- Pong1.py
+++ Pong1.py
@@ -35,6 +35,15 @@ ball.goto(0, 0)
 ball.dx = 0.3
 ball.dy = -0.3
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 
 # Function
 def paddle_a_up():
